8dee19b7f9fe7410dd128ba6889a47c025b1aa89
aksdad <akshay@aksdad.com>
2020-07-23 15:59:22 -0700

    add readme

--- Program.cs
+++ Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace waterBot
 {
     class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient _client;
+        private string _token;
+
+        public Program()
         {
-            Console.WriteLine("Hello World!");
+            _token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+        }
+
+        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            await _client.LoginAsync(TokenType.Bot, _token);
+            await _client.StartAsync();
+            _client.MessageReceived += MessageReceived;
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceived(SocketMessage msg)
+        {
+            if (msg.Content == "!gimmewater")
+            {
+                await msg.Channel.SendMessageAsync("/tts here's ur water u thirsty hoe 💦");
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,30 @@
+# Water bot for Discord
+- [Water bot for Discord](#water-bot-for-discord)
+  - [Introduction](#introduction)
+  - [Features](#features)
+  - [Roadmap](#roadmap)
+  - [Team](#team)
+  - [Libraries](#libraries)
+## Introduction
+
+Water reminder bot for discord servers.
+
+## Features
+
+- Water reminder every 10 minutes
+
+## Roadmap
+
+- Subscribe to notification
+- Custom settings per user
+- Start/end gaming session
+- Settable command character
+
+## Team
+
+- Akshay Dadhwal (aksdad)
+- Sahil Guleria? (darkyldull)
+
+## Libraries
+
+- [Discord.Net](https://github.com/discord-net/Discord.Net)
